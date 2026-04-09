8d3a6d1354be2e3145d2c703113ff9411ec85f97
janinevallente <janinexvallente@gmail.com>
2026-04-10 01:02:04 +0800

    enh: simplify RootLayout function signature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,7 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body className="antialiased">{children}</body>
